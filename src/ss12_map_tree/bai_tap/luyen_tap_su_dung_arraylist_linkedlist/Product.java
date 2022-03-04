bda384c6027e17243e4a3bbdde45bde736fa5168
chaumy791993 <chaumy791993@gmail.com>
2022-03-04 19:24:11 +0700

    upbai13

--- src/ss12_map_tree/bai_tap/luyen_tap_su_dung_arraylist_linkedlist/Product.java
+++ src/ss12_map_tree/bai_tap/luyen_tap_su_dung_arraylist_linkedlist/Product.java
@@ -1,26 +1,25 @@
 package ss12_map_tree.bai_tap.luyen_tap_su_dung_arraylist_linkedlist;
 
 public class Product {
-    private int productId;
+    private String productId;
     private String productName;
     private double productPrice;
 
-
-    public Product() {
-
-    }
-
-    public Product(int productId, String productName, double productPrice) {
+    public Product(String productId, String productName, double productPrice) {
         this.productId = productId;
         this.productName = productName;
         this.productPrice = productPrice;
     }
 
-    public int getProductId() {
+    public Product() {
+
+    }
+
+    public String getProductId() {
         return productId;
     }
 
-    public void setProductId(int productId) {
+    public void setProductId(String productId) {
         this.productId = productId;
     }
 
@@ -43,9 +42,9 @@ public class Product {
     @Override
     public String toString() {
         return "Product{" +
-                "productId=" + productId +
-                ", productName='" + productName + '\'' +
-                ", productPrice=" + productPrice +
+                "productId: " + productId +
+                ", productName: " + productName +
+                ", productPrice: " + productPrice +
                 '}';
     }
 }
---
+++ src/ss12_map_tree/bai_tap/luyen_tap_su_dung_arraylist_linkedlist/ProductManager.java
@@ -0,0 +1,12 @@
+package ss12_map_tree.bai_tap.luyen_tap_su_dung_arraylist_linkedlist;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ProductManager {
+    static ArrayList<Product> productArrayList = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+static {
+    productArrayList.add(new Product(A001,"quần",5000));
+}
+}
